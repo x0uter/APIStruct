aea727adf1e8792f50b061a08ecbeafba0190cd8
André <ancatos@outlook.com>
2018-07-18 17:37:00 +0100

    Added body parser

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs = require('hbs');
+var bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
@@ -17,6 +18,8 @@ hbs.registerPartials(__dirname + '/views/partials')
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
