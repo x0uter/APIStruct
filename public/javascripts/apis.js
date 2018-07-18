d7a35f7e7556f52c9e300a57859bb6d7c940c1b5
André <ancatos@outlook.com>
2018-07-19 00:21:17 +0100

    delete ajax request

--- public/javascripts/apis.js
+++ public/javascripts/apis.js
@@ -1,13 +1,7 @@
+/**
+ * table filter
+ */
 $(document).ready(function () {
-
-    $('.star').on('click', function () {
-        $(this).toggleClass('star-checked');
-    });
-
-    $('.ckbox label').on('click', function () {
-        $(this).parents('tr').toggleClass('selected');
-    });
-
     $('.btn-filter').on('click', function () {
         var $target = $(this).data('target');
         if ($target != 'all') {
@@ -19,3 +13,27 @@ $(document).ready(function () {
     });
 
 });
+
+/**
+ * delete route endpoint
+ */
+function deleteRoute(method, route) {
+    const data = {
+        method: method,
+        route: route
+    }
+    ajaxRequest('DELETE', '/deleteroute', JSON.stringify(data)).then(window.location.href='/');
+}
+
+function ajaxRequest(meth, path, data) {
+    return fetch(path, {
+        method: meth,
+        headers: { 'Content-Type': 'application/json' },
+        body: data,
+        credentials: 'same-origin'
+    })
+        .then(resp => {
+            if (resp.status != 200) throw new Error(resp.statusText)
+            return resp.text()
+        })
+}
